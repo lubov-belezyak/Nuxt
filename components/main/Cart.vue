cf3ccc5bb4422657178f1289d0841a1cf20466d3
Belezyak-Lubov <podpiski74@bk.ru>
2022-01-10 18:14:00 +0500

    update(): card

--- components/main/Cart.vue
+++ components/main/Cart.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <div class="relative flex gap-3 ">
-      <img :src="src" class="relative rounded-lg mb-4">
+  <div class="bg-back-card">
+    <div class="flex">
+      <img :src="src" class="rounded-lg mb-4">
       <div class="flex flex-col w-fullh-screen ">
         <div class="flex flex-row justify-between m-auto">
           <p class="relative text-xl whitespace-nowrap truncate overflow-hidden text-white">{{ title }}</p>
         </div>
       </div>
     </div>
-    <p class="-mt-4 text-gray-500">{{ text }}</p>
+    <p class="-mt-4 text-gray-500 pl-4 pr-4">{{ text }}</p>
   </div>
 </template>
 
